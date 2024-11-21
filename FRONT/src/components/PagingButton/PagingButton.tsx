ea35558c5c20a655fa6a7d8360ed12c733527f59
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-11-21 19:13:17 +0900

    refactor: 화살표를 누르면 5개 씩 이동하도록 바꿈 - arrow function를 function으로 변경 - 로직 변경: 화살표를 누르면 항상 5개씩 이동

--- FRONT/src/components/PagingButton/PagingButton.tsx
+++ FRONT/src/components/PagingButton/PagingButton.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { PagingButtonContainer, ArrowButton, PageButton } from "./PagingButton.styles";
 import leftArrow from "../../assets/icons/left-arrow.svg";
 import rightArrow from "../../assets/icons/right-arrow.svg";
@@ -9,71 +8,49 @@ interface PagingButtonProps {
   setCurrentPage: (page: number) => void;
 }
 
-const PagingButton: React.FC<PagingButtonProps> = ({
+function PagingButton({
   totalPages,
   currentPage,
   setCurrentPage,
-}) => {
-  const pagesArray = [];
-  
-  if (totalPages <= 5) {
-    pagesArray.push(...Array.from({ length: totalPages }, (_, index) => index + 1));
-    return (
-      <PagingButtonContainer>
-        {pagesArray.map((page, index) => (
-          <PageButton
-            key={index}
-            $isActive={currentPage === page}
-            onClick={() => setCurrentPage(page)}
-          >
-            {page}
-          </PageButton>
-        ))}
-      </PagingButtonContainer>
-    );
-  } else {
-    const leftLimit = Math.max(1, currentPage - 2);
-    const rightLimit = Math.min(totalPages, currentPage + 2);
+}: PagingButtonProps) {
 
-    if (currentPage <= 3) {
-      for (let i = 1; i <= 5 && i <= totalPages; i++) {
-        pagesArray.push(i);
-      }
-    } else if (currentPage >= totalPages - 2) {
-      for (let i = totalPages - 4; i <= totalPages; i++) {
-        pagesArray.push(i);
-      }
-    } else {
-      for (let i = leftLimit; i <= rightLimit; i++) {
-        pagesArray.push(i);
-      }
-    }
-    return (
-      <PagingButtonContainer>
+  const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1;
+  const endPage = Math.min(startPage + 4, totalPages);
+  console.log(startPage, endPage);
+
+  const pagesArray = Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
+
+  return (
+    <PagingButtonContainer>
+      {startPage > 1 && (
         <ArrowButton
-          onClick={() => setCurrentPage(currentPage - 1)}
+          onClick={() => setCurrentPage(startPage - 1)}
           disabled={currentPage === 1}
         >
           <img src={leftArrow} alt="left-arrow" />
         </ArrowButton>
-        {pagesArray.map((page, index) => (
-          <PageButton
-            key={index}
-            $isActive={currentPage === page}
-            onClick={() => setCurrentPage(page)}
-          >
-            {page}
-          </PageButton>
-        ))}
+      )}
+
+      {pagesArray.map((page) => (
+        <PageButton
+          key={page}
+          $isActive={currentPage === page}
+          onClick={() => setCurrentPage(page)}
+        >
+          {page}
+        </PageButton>
+      ))}
+
+      {endPage < totalPages && (
         <ArrowButton
-          onClick={() => setCurrentPage(currentPage + 1)}
+          onClick={() => setCurrentPage(endPage + 1)}
           disabled={currentPage === totalPages}
         >
           <img src={rightArrow} alt="right-arrow" />
         </ArrowButton>
-      </PagingButtonContainer>
-    );
-  }
-};
+      )}
+    </PagingButtonContainer>
+  );
+}
 
 export default PagingButton;
