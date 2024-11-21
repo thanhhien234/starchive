fa9b195915957fe93f38414ab9428202e8405a19
Jonghyeok <jhpmuscle@gmail.com>
2024-11-22 02:57:39 +0900

    refactor: 스타일 요소 분리
    - PostItem에 들어가는 스타일 파일 분리

---
+++ FRONT/src/pages/Home/components/PostItem/PostItem.style.ts
@@ -0,0 +1,68 @@
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.article`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 30px 5px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+
+  & > a {
+    text-decoration: none;
+  }
+`;
+
+export const Title = styled.h3`
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`
+
+export const UserProfileWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+  width: 100%;
+  align-items: center;
+`
+
+export const UserImage = styled.figure`
+  padding: 0;
+  margin: 0;
+  width: 35px;
+  height: 35px;
+  border-radius: 100%;
+  overflow: hidden;
+  display: flex:
+  justify-content: center;
+  aligh-items: center;
+  flex: none;
+`;
+
+export const UserInfoWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+`;
+
+export const UserNameWrapper = styled.div`
+  display: flex;
+  gap: 2px;
+`;
+
+export const UserName = styled.span`
+  font-size: 12px;
+`;
+
+export const SubInfo = styled.span`
+  font-size: 12px;
+  color: #AAA;
+`;
+
+export const Content = styled.p`
+  display: -webkit-box;
+  -webkit-line-clamp: 5;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
