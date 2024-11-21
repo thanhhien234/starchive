3e78e216ace73a9616aa39697921c7fcaa104770
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-11-21 19:19:23 +0900

    fix: remove console log

--- FRONT/src/components/PagingButton/PagingButton.tsx
+++ FRONT/src/components/PagingButton/PagingButton.tsx
@@ -16,8 +16,6 @@ function PagingButton({
 
   const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1;
   const endPage = Math.min(startPage + 4, totalPages);
-  console.log(startPage, endPage);
-
   const pagesArray = Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
 
   return (
