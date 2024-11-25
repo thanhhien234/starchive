88727efe82607b3077cf093a9132aa4e59dfdf7f
Yongjin An <yongjin5352@gmail.com>
2024-11-25 20:30:09 +0900

    feat: 카테고리별 게시글 조회 응답 형식 변경
    - 카테고리별 게시글 조회 api 응답에 카테고리 계층이 담기도록 수정
    - 게시글 내용 앞 350자가 전달되도록 수정
    
    Refs: #5

--- BACK/nest-app/src/post/post.service.ts
+++ BACK/nest-app/src/post/post.service.ts
@@ -6,6 +6,58 @@ export class PostService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getPostsByCategory(categoryId: bigint) {
+    const categoryTreeMap = await this.makeCategoryTreeMap();
+
+    const searchCategory: CategoryTree = categoryTreeMap.get(categoryId);
+
+    const posts = await this.prisma.post.findMany({
+      where: {
+        categoryId: {
+          in: searchCategory?.getAllDescendantIds() ?? [],
+        },
+      },
+    });
+
+    return this.toResponse(posts, categoryTreeMap);
+  }
+
+  public async getAllPosts() {
+    const categoryTreeMap = await this.makeCategoryTreeMap();
+
+    const posts = await this.prisma.post.findMany();
+
+    return this.toResponse(posts, categoryTreeMap);
+  }
+
+  private toResponse(
+    posts: {
+      postId: bigint;
+      categoryId: bigint;
+      title: string;
+      content: string;
+      author: string;
+      password: string;
+      createAt: Date;
+    }[],
+    categoryTreeMap: Map<BigInt, CategoryTree>,
+  ) {
+    return posts.map((post) => {
+      const categoryTree: CategoryTree = categoryTreeMap.get(post.categoryId);
+
+      const hierList: categoryResponse[] = categoryTree.getHierList();
+
+      return {
+        categoryHier: hierList,
+        postId: Number(post.postId),
+        author: post.author,
+        title: post.title,
+        content: post.content.slice(0, 350),
+        createdAt: post.createAt,
+      };
+    });
+  }
+
+  private async makeCategoryTreeMap() {
     const categoryMap = new Map<BigInt, CategoryTree>();
 
     const allCategories = await this.prisma.category.findMany();
@@ -19,6 +71,10 @@ export class PostService {
       const currentNode: CategoryTree = categoryMap.get(categoryIdLong);
       currentNode.fillNode(categoryIdLong, name);
 
+      if (!parentId) {
+        return;
+      }
+
       const parentIdLong = BigInt(parentId);
       if (!categoryMap.has(parentIdLong)) {
         categoryMap.set(parentIdLong, new CategoryTree());
@@ -26,28 +82,23 @@ export class PostService {
       const parentNode: CategoryTree = categoryMap.get(parentIdLong);
 
       parentNode.addChild(currentNode);
+      currentNode.addParent(parentNode);
     });
-
-    const searchCategory: CategoryTree = categoryMap.get(categoryId);
-
-    return this.prisma.post.findMany({
-      where: {
-        categoryId: {
-          in: searchCategory.getAllDescendantIds(),
-        },
-      },
-    });
+    return categoryMap;
   }
+}
 
-  async getAllPosts() {
-    return this.prisma.post.findMany();
-  }
+export interface categoryResponse {
+  categoryId: number;
+  categoryName: string;
 }
 
 class CategoryTree {
   private id: bigint;
-  private name: String;
+  private name: string;
   private children: CategoryTree[] = [];
+  private parent: CategoryTree;
+  private hierList: categoryResponse[] = [];
 
   public fillNode(id: bigint, name: string) {
     this.id = id;
@@ -58,6 +109,10 @@ class CategoryTree {
     this.children.push(childNode);
   }
 
+  public addParent(parentNode: CategoryTree) {
+    this.parent = parentNode;
+  }
+
   public getAllDescendantIds(): bigint[] {
     const idList: bigint[] = [this.id];
 
@@ -66,5 +121,22 @@ class CategoryTree {
     });
 
     return idList;
+  }
+
+  public getHierList(): categoryResponse[] {
+    if (this.hierList.length !== 0) {
+      return this.hierList;
+    }
+
+    if (!!this.parent) {
+      this.hierList.push(...this.parent.getHierList());
+    }
+
+    this.hierList.push({
+      categoryId: Number(this.id),
+      categoryName: this.name,
+    });
+
+    return this.hierList;
   }
 }
