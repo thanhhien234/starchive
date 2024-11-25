45470a017604009a7b35309ac7aba5aa208b7c15
peanut990 <peanut1998@naver.com>
2024-11-26 03:08:32 +0900

    feat: 스웨거 정보 추가 (#5)
    Refs: #5

--- BACK/spring-app/src/main/java/com/starchive/springapp/category/controller/CategoryController.java
+++ BACK/spring-app/src/main/java/com/starchive/springapp/category/controller/CategoryController.java
@@ -2,16 +2,20 @@ package com.starchive.springapp.category.controller;
 
 import com.starchive.springapp.category.dto.CategoryListTreeResponse;
 import com.starchive.springapp.category.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "카테고리")
 public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/categorys")
+    @Operation(summary = "카테고리 목록 전체 조회")
     public CategoryListTreeResponse showCategories() {
         return categoryService.findAll();
     }
--- BACK/spring-app/src/main/java/com/starchive/springapp/category/dto/CategoryDto.java
+++ BACK/spring-app/src/main/java/com/starchive/springapp/category/dto/CategoryDto.java
@@ -1,6 +1,7 @@
 package com.starchive.springapp.category.dto;
 
 import com.starchive.springapp.category.domain.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -9,8 +10,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CategoryDto {
+    @Schema(description = "카테고리 식별 id 값", example = "1")
     private Long id;
+    @Schema(description = "카테고리 이름", example = "알고리즘")
     private String name;
+    @Schema(description = "하위 카테고리 목록", example = "{"
+            + "          \"id\": 2,\n"
+            + "          \"name\": \"자료구조\",\n"
+            + "          \"children\": null\n"
+            + "        }")
     private List<CategoryDto> children;
 
     public static CategoryDto from(Category category) {
