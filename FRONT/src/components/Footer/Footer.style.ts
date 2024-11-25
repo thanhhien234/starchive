2d44901dfa4d2621581e88be64a8e19f3a6a5bfb
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-11-25 10:06:05 +0900

    fix: 띄어쓰기 수정

--- FRONT/src/components/Footer/Footer.style.ts
+++ FRONT/src/components/Footer/Footer.style.ts
@@ -16,103 +16,103 @@ export const FooterContainer = styled.footer`
 `;
 
 export const InfoContainer = styled.section`
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
 
-    @media (max-width: 470px) {
-        flex-direction: column;
-        align-items: flex-start;
-        gap: 10px;
-    }
+  @media (max-width: 470px) {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+  }
 `;
 
 export const LeftContainer = styled.article`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
 
 export const LogoWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 3px;
-    color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 3px;
+  color: #fff;
 
-    img {
-        width: 20px;
-        height: 20px;
-    }
-    
-    h4 {
-        font-weight: 700;
-        margin: 0;
-    }
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  h4 {
+    font-weight: 700;
+    margin: 0;
+  }
 `;
 
 export const TextWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 3px;
-    font-size: 12px;
-    margin-left: 23px;
+  display: flex;
+  flex-direction: column;
+  gap: 3px;
+  font-size: 12px;
+  margin-left: 23px;
 
-    @media (max-width: 470px) {
-        margin-left: 0;
-    }
+  @media (max-width: 470px) {
+    margin-left: 0;
+  }
 `;
 
 export const CopyRight = styled.p`
-    font-size: 11px;
-    margin: 0;
+  font-size: 11px;
+  margin: 0;
 `;
 
 export const RightContainer = styled.article`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    align-items: flex-end;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: flex-end;
 
-    img {
-        width: 28px;
-        height: 28px;
-        cursor: pointer;
-    }
+  img {
+    width: 28px;
+    height: 28px;
+    cursor: pointer;
+  }
 
-    @media (max-width: 470px) {
-        flex-direction: row;
-        align-items: center;
-    }
+  @media (max-width: 470px) {
+    flex-direction: row;
+    align-items: center;
+  }
 `;
 
 export const FooterLinkList = styled.ul`
-    display: flex;
-    flex-direction: row;
-    list-style: none;
-    gap: 10px;
-    margin: 0;
-    padding: 0;
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  gap: 10px;
+  margin: 0;
+  padding: 0;
 `;
 
 export const FooterLink = styled.li`
-    font-size: 13px;
-    cursor: pointer;
-    &:hover {
-        text-decoration: underline;
-    }
-    & > a {
-        text-decoration: none;
-        color: var(--footer-text-color);
-    }
+  font-size: 13px;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+  & > a {
+    text-decoration: none;
+    color: var(--footer-text-color);
+  }
 `;
 
 export const Separator = styled.hr`
-    width: 1px;
-    height: 15px;
-    background-color: var(--footer-text-color);
-    border: none;
-    margin: 0;
+  width: 1px;
+  height: 15px;
+  background-color: var(--footer-text-color);
+  border: none;
+  margin: 0;
 `;
