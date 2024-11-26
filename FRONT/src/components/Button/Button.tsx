77ece5c0eeb075169ed6807e006c8e86e69cee54
Jonghyeok <jhpmuscle@gmail.com>
2024-11-26 16:54:40 +0900

    chore: 불필요한 import 제거
    - setup.ts에 필요없는 import 문 제거
    - 버튼 인터페이스의 함수 return 타입 정의

--- FRONT/src/components/Button/Button.tsx
+++ FRONT/src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import { Wrapper } from "./Button.style";
 interface ButtonProps {
   content: string;
   type: string;
-  handleButtonClick: Function | undefined;
+  handleButtonClick: () => void | undefined;
 }
 
 function Button({ content, type, handleButtonClick }: ButtonProps) {
