0a0b47de00fb9f703bfcd9f5d5ddf2c1627fc519
hanguswls <hanguswls@naver.com>
2024-11-27 22:47:31 +0900

    style: Category.ts를 category.ts로 파일 이름 변경

--- FRONT/src/components/Aside/Aside.tsx
+++ FRONT/src/components/Aside/Aside.tsx
@@ -2,7 +2,7 @@ import CategoryNode from './components/CategoryNode';
 import cancelButton from '../../assets/icons/cancel-button.svg'
 import logoIcon from '../../assets/logo/logo.svg';
 import useAside from './useAside';
-import { Category } from './types/Category';
+import { Category } from './types/category';
 import {
   Wrapper,
   Overlay,
