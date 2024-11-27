45cb07be95bb3da48b6534448894085299d891c7
peanut990 <peanut1998@naver.com>
2024-11-28 00:10:19 +0900

    refactor: 카테고리 테이블이름을 ERD 정의에 맞게 변경 (#5)
    Category -> Categorys
    
    Refs: #5

--- BACK/spring-app/src/main/java/com/starchive/springapp/category/domain/Category.java
+++ BACK/spring-app/src/main/java/com/starchive/springapp/category/domain/Category.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Table(name = "Categorys")
 public class Category {
     @Id
     @GeneratedValue
