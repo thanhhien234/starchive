993e02e3f75a287c13b0007d199d4a61826485b3
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-11-28 20:01:10 +0900

    feat: TagWrapper 컴포넌트 구현

---
+++ FRONT/src/components/TagWrapper/TagWrapper.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Tag = styled.span`
+  display: inline-block;
+  padding: 2px 5px;
+  background-color: var(--sub-color);
+  color: var(--text-color);
+  width: fit-content;
+  margin-right: 5px;
+  margin-bottom: 5px;
+`;
+
+export const TagList = styled.div`
+    display: inline-block;
+`;
