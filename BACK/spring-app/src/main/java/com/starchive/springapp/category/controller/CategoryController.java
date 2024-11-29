4ce2649902d129115626f2be89b2c3196cd5200d
peanut990 <peanut1998@naver.com>
2024-11-29 15:00:36 +0900

    feat: 특정 카테고리에 해당하는 해쉬태그 목록 조회 컨트롤러 구현 (#5)
    - 카테고리로 해쉬태그 조회 api 완성
    - 카테고리 api 통합테스트 작성
    
    Refs: #5

--- BACK/spring-app/src/main/java/com/starchive/springapp/category/controller/CategoryController.java
+++ BACK/spring-app/src/main/java/com/starchive/springapp/category/controller/CategoryController.java
@@ -2,10 +2,16 @@ package com.starchive.springapp.category.controller;
 
 import com.starchive.springapp.category.dto.CategoryListTreeResponse;
 import com.starchive.springapp.category.service.CategoryService;
+import com.starchive.springapp.global.dto.ResponseDto;
+import com.starchive.springapp.hashtag.dto.HashTagDto;
+import com.starchive.springapp.hashtag.service.HashTagService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "카테고리")
 public class CategoryController {
     private final CategoryService categoryService;
+    private final HashTagService hashTagService;
 
     @GetMapping("/categorys")
     @Operation(summary = "카테고리 목록 전체 조회")
     public CategoryListTreeResponse showCategories() {
         return categoryService.findAll();
+    }
+
+    @GetMapping("/categorys/{categoryId}/hashtags")
+    @Operation(summary = "특정 카테고리에 포함되는 해쉬태그 목록 조회")
+    public ResponseEntity<ResponseDto<List<HashTagDto>>> showHashTags(@PathVariable("categoryId") Long categoryId) {
+        List<HashTagDto> categorys = hashTagService.findManyByCategory(categoryId);
+        ResponseDto<List<HashTagDto>> listResponseDto = new ResponseDto<>(categorys);
+        return ResponseEntity.ok(listResponseDto);
     }
 }
--- BACK/spring-app/src/test/java/com/starchive/springapp/category/controller/CategoryControllerTest.java
+++ BACK/spring-app/src/test/java/com/starchive/springapp/category/controller/CategoryControllerTest.java
@@ -6,9 +6,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.starchive.springapp.category.domain.Category;
 import com.starchive.springapp.category.repository.CategoryRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.starchive.springapp.hashtag.domain.HashTag;
+import com.starchive.springapp.post.Post;
+import com.starchive.springapp.posthashtag.PostHashTag;
+import jakarta.persistence.EntityManager;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -18,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class CategoryControllerTest {
     @Autowired
     CategoryRepository categoryRepository;
@@ -25,8 +32,12 @@ class CategoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void init() {
+    @Autowired
+    EntityManager entityManager;
+
+
+    @Test
+    public void 목록_전체_조회_테스트() throws Exception {
         //given
         Category parent = new Category("알고리즘", null);
         Category child1 = new Category("자료구조", parent);
@@ -38,10 +49,7 @@ class CategoryControllerTest {
         categoryRepository.save(child2);
         categoryRepository.save(parent1);
         categoryRepository.save(child3);
-    }
 
-    @Test
-    public void 목록_전체_조회_테스트() throws Exception {
         // when
         mockMvc.perform(get("/categorys")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -54,5 +62,49 @@ class CategoryControllerTest {
                 .andExpect(jsonPath("$.roots[1].name").value("프로젝트"))
                 .andExpect(jsonPath("$.roots[1].children[0].name").value("요구사항"));
 
+    }
+
+    @Test
+    public void 특정_카테고리에_포함되는_해쉬태그_목록_조회_통합_테스트() throws Exception {
+        // Given
+        Category category = new Category("알고리즘", null);
+        entityManager.persist(category);
+
+        Post post = Post.builder()
+                .title("알고리즘 기초")
+                .content("알고리즘을 학습합시다.")
+                .author("홍길동")
+                .password("1234")
+                .dateTime(LocalDateTime.now())
+                .category(category)
+                .build();
+        entityManager.persist(post);
+
+        HashTag hashTag1 = new HashTag("자료구조");
+        HashTag hashTag2 = new HashTag("다이나믹 프로그래밍");
+        entityManager.persist(hashTag1);
+        entityManager.persist(hashTag2);
+
+        PostHashTag postHashTag1 = PostHashTag.builder()
+                .post(post)
+                .hashTag(hashTag1)
+                .build();
+        entityManager.persist(postHashTag1);
+
+        PostHashTag postHashTag2 = PostHashTag.builder()
+                .post(post)
+                .hashTag(hashTag2)
+                .build();
+        entityManager.persist(postHashTag2);
+
+        // When & Then
+        mockMvc.perform(get("/categorys/{categoryId}/hashtags", category.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data[0].id").value(hashTag1.getId()))
+                .andExpect(jsonPath("$.data[0].name").value(hashTag1.getName()))
+                .andExpect(jsonPath("$.data[1].id").value(hashTag2.getId()))
+                .andExpect(jsonPath("$.data[1].name").value(hashTag2.getName()));
     }
 }
