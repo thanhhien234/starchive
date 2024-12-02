64af5eb9723669fa4b33d54756422b55fb1ab989
hanguswls <hanguswls@naver.com>
2024-12-03 06:58:04 +0900

    feat: 카테고리 데이터 API 연동
    - Aside 컴포넌트의 카테고리 데이터를 API 호출로 변경
    - 기존 목데이터를 public 디렉토리로 이동

--- FRONT/src/components/Aside/useAside.ts
+++ FRONT/src/components/Aside/useAside.ts
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import category from './category.example.json';
 import useAsideStore from '../../store/useAsideStore';
-import { CategoryId } from '../../types/category';
+import { Category, CategoryId } from '../../types/category';
+import { fetchCategories } from '../../services/categoryApi';
+import { useQuery } from '@tanstack/react-query';
+import { ApiResponse } from '../../services/api';
 
 function useAside() {
   const navigate = useNavigate();
   const { isAsideOpen, setIsAsideOpen } = useAsideStore();
   const [activeCategoryId, setActiveCategoryId] = useState<CategoryId>(undefined);
+  const { data } = useQuery<ApiResponse<Category[]>>({ queryKey: ['categories'], queryFn: () => fetchCategories()});
+  const category = data?.data;
 
   const handleCategorySelect = (categoryId: CategoryId) => {
     setActiveCategoryId(categoryId);
