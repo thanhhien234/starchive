47e4111952083ab659bae5b3a06a978487f39739
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-12-06 22:06:21 +0900

    fix: 태그별 글 목록 조회 api 수정

--- FRONT/src/services/tagApi.ts
+++ FRONT/src/services/tagApi.ts
@@ -1,7 +1,5 @@
 import { getRequest } from "./api";
-import { Post } from "../types/post";
 import { Tag } from "../types/tag";
 
-export const fetchPostsByTag = (tagName: string) => getRequest<Post[]>('/hashtag', { params: { tag: tagName } });
 export const fetchAllTagList = () => getRequest<Tag[]>('/hashtags');
 export const fetchTagListByCategory = (categoryId: number) => getRequest<Tag[]>(`/categorys/${categoryId}/hashtags`);
