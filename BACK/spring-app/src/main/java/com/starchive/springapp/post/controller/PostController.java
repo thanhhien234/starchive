bba322d561a9a666949e6598dbe584d875f7bad6
peanut990 <peanut1998@naver.com>
2025-01-03 14:47:07 +0900

    feat: 글 게시 기능 테스트 코드 작성, 스웨거 추가 (#4)
    Refs: #4

--- BACK/spring-app/src/main/java/com/starchive/springapp/post/controller/PostController.java
+++ BACK/spring-app/src/main/java/com/starchive/springapp/post/controller/PostController.java
@@ -2,6 +2,8 @@ package com.starchive.springapp.post.controller;
 
 import com.starchive.springapp.post.dto.PostCreateRequest;
 import com.starchive.springapp.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "게시글")
 public class PostController {
     private final PostService postService;
 
     @PostMapping("/post")
+    @Operation(summary = "게시글 작성")
     public ResponseEntity<?> post(@Valid @RequestBody PostCreateRequest request) {
 
         postService.createPost(request);
