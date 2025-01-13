0defeddd01a876f2bf277c1a2ce2339238d0b9fc
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2025-01-13 11:29:54 +0900

    feat: Post 페이지를 route에 추가(#6)

--- FRONT/src/App.tsx
+++ FRONT/src/App.tsx
@@ -6,7 +6,8 @@ import Navbar from '@_components/Navbar/Navbar'
 import Footer from '@_components/Footer/Footer'
 import Aside from '@_components/Aside/Aside'
 import useLoadingStore from './store/useLoadingStore';
-import LoadingModal from '@_components/LoadingModal/LoadingModal'
+import LoadingModal from '@_components/LoadingModal/LoadingModal';
+import Post from './pages/Post/Post';
 
 function App() {
   const { isLoading } = useLoadingStore();
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/:page?' element={<Home />} />
         <Route path='/create-post' element={<CreatePost />} />
+        <Route path='/post' element={<Post />} />
       </Routes>
       <Footer />
       <LoadingModal isLoading={isLoading} />
