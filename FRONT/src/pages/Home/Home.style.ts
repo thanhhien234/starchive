c45a8ccb40a3eaedde58f548bee4ce7f7716170e
hanguswls <hanguswls@naver.com>
2025-01-21 12:07:39 +0900

    refactor: Home.tsx에서 글 목록과 태그 컨테이너 반응형 디자인 적용 - Refs: #69

--- FRONT/src/pages/Home/Home.style.ts
+++ FRONT/src/pages/Home/Home.style.ts
@@ -1,24 +1,32 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-  max-width: 700px;
+  display: flex;
+  flex-direction: column;
+  max-width: 1080px;
   padding: 30px 20px;
+  gap: 10px;
   margin: auto;
 `;
 
 export const ButtonWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-bottom: 10px;
 `;
 
 export const PostListWithTags = styled.div`
   display: flex;
   gap: 20px;
+
+  @media (max-width: 470px) {
+    flex-direction: column;
+    gap: 10px;
+  }
 `;
 
 export const PostItemContainer = styled.section`
   width: 100%;
+  max-width: 700px;
 `;
 
 export const PagingButtonWrapper = styled.section`
